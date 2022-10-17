0ebb512e1d94be181bfb3c8dffdeda85e5791049
baothi <baothi246@gmail.com>
2022-10-17 22:47:07 +0700

    multiple input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Setup from './tutorial/4-forms/setup/1-controlled-inputs';
+import Setup from './tutorial/4-forms/setup/2-multiple-inputs';
 // import Final from './tutorial/3-conditional-rendering/final/1-multiple-returns';
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <h2>Forms</h2>
       <Setup />
     </div>
   )
--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,58 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    // console.log('e.target.value : ',firstName, email)
+    if(firstName && email){
+      const person = { id: new Date().getTime().toString(), firstName, email };
+      // console.log('person : ',person);
+      setPeople((people)=>{
+        return [...people, person]
+       })
+       setFirstName('');
+       setEmail('');
+    }else{
+      console.log('email : ',email);
+    }
+  };
+  return (
+    <>
+      <article>
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <label htmlFor="firstName">Name: </label>
+            <input type="text" className="form-control"
+              id="firstName" value={firstName} name='firstName'
+              onChange={(e) => setFirstName(e.target.value)}
+              placeholder="First Name"
+            />
+          </div>
+          <div className='form-control'>
+            <label htmlFor="email">Email: </label>
+            <input type="text" className="form-control"
+              id="email" value={email} name='email'
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">Add Person</button>
+        </form>
+        {people.map((person, index) => {
+          const {id, firstName, email } = person;
+          return (
+            <div className='item' id1={id} key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
