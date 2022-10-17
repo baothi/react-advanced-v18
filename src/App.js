57d56eb96aaf8ac3636c06e7f3469ecbf5a9bf0f
baothi <baothi246@gmail.com>
2022-10-17 23:05:15 +0700

    useRef

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/4-forms/setup/2-multiple-inputs';
+import Setup from './tutorial/5-useRef/setup/1-useRef-basics';
 // import Final from './tutorial/3-conditional-rendering/final/1-multiple-returns';
 function App() {
   return (
