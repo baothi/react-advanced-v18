d62928fce183aad896c0c9ed721cf24cca3d0574
baothi <baothi246@gmail.com>
2022-10-18 18:03:35 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Setup from './tutorial/10-prop-types/setup';
-import Final from './tutorial/10-prop-types/final';
+import Setup from './tutorial/12-memo-useMemo-useCallback/setup/index';
+import Final from './tutorial/12-memo-useMemo-useCallback/final/index';
 function App() {
   return (
     <div className='container'>
