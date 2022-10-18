82768d1efc6cfeca5adf72236909321d660f6b77
baothi <baothi246@gmail.com>
2022-10-18 11:48:07 +0700

    create modal use reducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Setup from './tutorial/5-useRef/setup/1-useRef-basics';
-// import Final from './tutorial/3-conditional-rendering/final/1-multiple-returns';
+import Setup from './tutorial/6-useReducer/setup';
+import Final from './tutorial/6-useReducer/final';
 function App() {
   return (
     <div className='container'>
-      <h2>Forms</h2>
+      <h2>useReducer</h2>
       <Setup />
     </div>
   )
